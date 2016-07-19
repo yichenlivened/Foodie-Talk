7d85bc630b233924951ef14ef71ef8b8bb2cb04a
Yi Chen <easoncy435@gmail.com>
2016-07-18 23:49:25 -0400

    foursquare API

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 angular
-  .module('foodieTalkApp', [
+.module('foodieTalkApp', [
     'ngAnimate',
     'ngAria',
     'ngCookies',
@@ -19,7 +19,7 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
+.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -34,4 +34,56 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  });
+})
+
+.factory('myService',function($http){
+  var auth = {
+    clienId: '222LJGYTFP5HPMFVJLICYJ4UBWTBBQJRHPXI404GZEI3MT5T',
+    clientSecret: 'Y1TYEO5ZFREPIRXHVB5O1TSU1OGEH3K0DCWQXSR2NZKR4T1K'
+  };
+
+  function getToday(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+    var yyyy = today.getFullYear();
+
+    if(dd<10) {
+      dd='0'+dd;
+    }
+
+    if(mm<10) {
+      mm='0'+mm;
+    }
+
+    today = yyyy+mm+dd;
+    return today;
+  }
+
+  var myService = {
+    async: function(ll,section){
+      var data = {
+        client_id: auth.clienId,
+        client_secret: auth.clientSecret,
+        ll:ll,
+        section: section,
+        venuePhotos:1,
+        v: getToday()
+      };
+
+      var promise = $http({
+        method: 'GET',
+        url: 'https://api.foursquare.com/v2/venues/explore',
+        params: data
+      }).success(function (response) {
+        return response.data;
+      }).error(function (error) {
+        console.log(error);
+      });
+
+      return promise;
+    }
+  };
+
+  return myService;
+});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,21 @@
  * Controller of the foodieTalkApp
  */
 angular.module('foodieTalkApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, myService) {
+    navigator.geolocation.getCurrentPosition(function(position){
+        $scope.$apply(function(){
+          $scope.position = position;
+          var section = 'food';
+          var ll = $scope.position.coords.latitude + ',' +$scope.position.coords.longitude;
+          myService.async(ll, section).then(function(response){
+            $scope.data = response.data.response;
+            console.log($scope.data);
+            $scope.restaurants = $scope.data.groups[0].items;
+            console.log($scope.restaurants);
+          });
+        });
+    });
+
+
+
   });
