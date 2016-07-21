21bde7f8c0a6a8b596a9f316fae2830aa4a17083
Yi Chen <easoncy435@gmail.com>
2016-07-21 19:31:41 -0400

    grunt bug fix

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 angular
-.module('foodieTalkApp', [
+  .module('foodieTalkApp', [
     'ngAnimate',
     'ngAria',
     'ngCookies',
@@ -19,7 +19,7 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-.config(function ($routeProvider) {
+  .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -39,152 +39,148 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-})
+  })
 
-.factory('myService',function($http){
-  var auth = {
-    clienId: '222LJGYTFP5HPMFVJLICYJ4UBWTBBQJRHPXI404GZEI3MT5T',
-    clientSecret: 'Y1TYEO5ZFREPIRXHVB5O1TSU1OGEH3K0DCWQXSR2NZKR4T1K'
-  };
+  .factory('myService', function ($http) {
+    var auth = {
+      clienId: '222LJGYTFP5HPMFVJLICYJ4UBWTBBQJRHPXI404GZEI3MT5T',
+      clientSecret: 'Y1TYEO5ZFREPIRXHVB5O1TSU1OGEH3K0DCWQXSR2NZKR4T1K'
+    };
 
-  function getToday(){
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
+    function getToday() {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1; //January is 0!
+      var yyyy = today.getFullYear();
 
-    if(dd<10) {
-      dd='0'+dd;
-    }
-
-    if(mm<10) {
-      mm='0'+mm;
-    }
-
-    today = yyyy+mm+dd;
-    return today;
-  }
-
-  var myService = {
-    asyncRestaurantList: function(ll,section){
-      var data = {
-        client_id: auth.clienId,
-        client_secret: auth.clientSecret,
-        ll:ll,
-        section: section,
-        venuePhotos:1,
-        v: getToday(),
-        limit: 30
-      };
-
-      var promise = $http({
-        method: 'GET',
-        url: 'https://api.foursquare.com/v2/venues/explore',
-        params: data
-      }).success(function (response) {
-        return response.data;
-      }).error(function (error) {
-        console.log(error);
-      });
-
-      return promise;
-    },
-
-    asyncRestaurant: function(id){
-      var data = {
-        client_id: auth.clienId,
-        client_secret: auth.clientSecret,
-        v: getToday()
-      };
-
-      var promise = $http({
-        method: 'GET',
-        url: 'https://api.foursquare.com/v2/venues/'+id,
-        params: data
-      }).success(function (response) {
-        return response.data;
-      }).error(function (error) {
-        console.log(error);
-      });
-
-      return promise;
-    }
-  };
-
-  return myService;
-})
-  .filter('price', filterPrice)
-  .filter('comment', filterComment)
-  .directive('starRating', starRating);
-
-function filterPrice(){
-  return function(tier, currency) {
-    var i, price = '';
-    for(i=0; i<parseInt(tier); i++){
-      price = price.concat(currency);
-    }
-    return price;
-  }
-
-}
-function filterComment(){
-  return function(text) {
-    return '"'+text+'"';
-  }
-}
-
-function starRating(){
-  return {
-    restrict:'AE',
-    template:
-    '<ul class="star-rating" tabindex="{{readonly ? 0 : -1}}" aria-label="{{ratingValue}} star" ng-class="{readonly: readonly}">' +
-    '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-click="toggle($index)" tabindex="{{readonly ? -1 : 0}}">' +
-    '    <i class="fa fa-star" aria-label="{{$index+1}} star"></i>' + // or &#973
-    '  </li>' +
-    '</ul>',
-    scope: {
-      ratingValue: '=?ngModel',
-      max: '=?', // optional (default is 5)
-      onRatingSelect: '&?',
-      readonly: '=?'
-    },
-    link: function(scope, element, attributes){
-      if (scope.max == undefined) {
-        scope.max = 5;
+      if (dd < 10) {
+        dd = '0' + dd;
       }
-      if (scope.ratingValue == undefined) {
-        scope.ratingValue = Math.floor((Math.random() * 5) + 1);
+
+      if (mm < 10) {
+        mm = '0' + mm;
       }
-      console.log(scope.ratingValue);
-      function updateStars() {
-        scope.stars = [];
-        for (var i = 0; i < scope.max; i++) {
-          if(scope.readonly == undefined || scope.readonly === false){
+
+      today = yyyy + mm + dd;
+      return today;
+    }
+
+    var myService = {
+      asyncRestaurantList: function (ll, section) {
+        // jscs:disable
+        var data = {
+          client_id: auth.clienId,
+          client_secret: auth.clientSecret,
+          ll: ll,
+          section: section,
+          venuePhotos: 1,
+          v: getToday(),
+          limit: 30
+        };
+
+        var promise = $http({
+          method: 'GET',
+          url: 'https://api.foursquare.com/v2/venues/explore',
+          params: data
+        }).success(function (response) {
+          return response.data;
+        }).error(function (error) {
+          console.log(error);
+        });
+
+        return promise;
+      },
+
+      asyncRestaurant: function (id) {
+        var data = {
+          client_id: auth.clienId,
+          client_secret: auth.clientSecret,
+          v: getToday()
+        };
+
+        var promise = $http({
+          method: 'GET',
+          url: 'https://api.foursquare.com/v2/venues/' + id,
+          params: data
+        }).success(function (response) {
+          return response.data;
+        }).error(function (error) {
+          console.log(error);
+        });
+
+        return promise;
+      }
+    };
+
+    return myService;
+  })
+  .filter('price', function () {
+    return function (tier, currency) {
+      var i, price = '';
+      for (i = 0; i < parseInt(tier); i++) {
+        price = price.concat(currency);
+      }
+      return price;
+    };
+  })
+  .filter('comment', function () {
+    return function (text) {
+      return '"' + text + '"';
+    };
+  })
+  .directive('starRating', function () {
+    return {
+      restrict: 'AE',
+      template: '<ul class="star-rating" tabindex="{{readonly ? 0 : -1}}" aria-label="{{ratingValue}} star" ng-class="{readonly: readonly}">' +
+      '  <li ng-repeat="star in stars" class="star" ng-class="{filled: star.filled}" ng-click="toggle($index)" tabindex="{{readonly ? -1 : 0}}">' +
+      '    <i class="fa fa-star" aria-label="{{$index+1}} star"></i>' + // or &#973
+      '  </li>' +
+      '</ul>',
+      scope: {
+        ratingValue: '=?ngModel',
+        max: '=?', // optional (default is 5)
+        onRatingSelect: '&?',
+        readonly: '=?'
+      },
+      link: function (scope) {
+        if (scope.max === undefined) {
+          scope.max = 5;
+        }
+        if (scope.ratingValue === undefined) {
+          scope.ratingValue = Math.floor((Math.random() * 5) + 1);
+        }
+        console.log(scope.ratingValue);
+        function updateStars() {
+          scope.stars = [];
+          for (var i = 0; i < scope.max; i++) {
+            if (scope.readonly === undefined || scope.readonly === false) {
               scope.stars.push({
                 filled: i < Math.floor(scope.ratingValue)
               });
-          } else {
-            scope.stars.push({
+            } else {
+              scope.stars.push({
                 filled: i < Math.floor(scope.ratingValue)
-            });
+              });
+            }
+
           }
+        }
 
-        }
-      };
+        scope.toggle = function (index) {
+          if (scope.readonly === undefined || scope.readonly === false) {
+            scope.ratingValue = index + 1;
+            scope.onRatingSelect = function () {
+              var rating = index + 1;
+              return rating;
+            };
+          }
+        };
 
-      scope.toggle = function(index) {
-        if (scope.readonly == undefined || scope.readonly === false){
-          scope.ratingValue = index + 1;
-          scope.onRatingSelect = function(){
-              rating: index + 1;
-          };
-        }
-      };
-      scope.$watch('ratingValue', function(oldValue, newValue) {
-        if (newValue) {
-          updateStars();
-        }
-      });
-    }
-  }
-}
+        scope.$watch('ratingValue', function (oldValue, newValue) {
+          if (newValue) {
+            updateStars();
+          }
+        });
+      }
+    };
+  });
